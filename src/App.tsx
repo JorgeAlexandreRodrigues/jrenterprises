929446717e59ab28366f04023497225a68db57fb
Jorge Rodrigues <a20632@alunos.ipca.pt>
2024-02-22 22:16:43 +0000

    ref: add investment credit component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import { useState } from "react";
-import { Container, Grid } from "@mui/material";
+import { Container} from "@mui/material";
 import Navbar from "./components/Navbar";
-import Result from "./components/Result";
-import SliderSelect from "./components/SliderSelect";
-
+import InvestimentCredit from "./components/InvestmentCredit/InvestimentCredit";
 
 function App() {
 
-  const [data, setData] = useState({
-    vF: 50000,
-    eM: 600,
-    cG: 1000
-  })
   return (
     <div className="App">
       <Navbar />
       <Container maxWidth="xl" sx={{ mt: 4 }}>
-        <Grid container spacing={5} alignItems='center'>
-          <Grid item xs={12} md={6}>
-            <SliderSelect data={data} setData={setData} />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Result data={data} />
-          </Grid>
-        </Grid>
+        <InvestimentCredit />
       </Container>
     </div>
   );
