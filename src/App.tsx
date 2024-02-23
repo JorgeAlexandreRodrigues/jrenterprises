89f93ccbe3beceff3f6396aab673a9316b4737bd
Jorge Rodrigues <a20632@alunos.ipca.pt>
2024-02-23 22:31:14 +0000

    feat: add doe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import { Container} from "@mui/material";
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { Container } from "@mui/material";
 import Navbar from "./components/Navbar";
 import InvestimentCredit from "./components/InvestmentCredit/InvestimentCredit";
+import Doe from "./components/Doe/Doe";
 
 function App() {
-
   return (
-    <div className="App">
-      <Navbar />
-      <Container maxWidth="xl" sx={{ mt: 4 }}>
-        <InvestimentCredit />
-      </Container>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Container maxWidth="xl" sx={{ mt: 4 }}>
+          <Routes>
+            <Route path="/investment-credit" element={<InvestimentCredit />} />
+            <Route path="/doe" element={<Doe />} />
+          </Routes>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
