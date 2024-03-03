e417cd16507a42b76dff1c9b3d4ced7fbf7898db
Jorge Rodrigues <a20632@alunos.ipca.pt>
2024-03-03 17:02:04 +0000

    feat: home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from "@mui/material";
 import Navbar from "./components/Navbar";
+import Home from './components/Home/Home';
 import InvestimentCredit from "./components/InvestmentCredit/InvestimentCredit";
 import Doe from "./components/Doe/Doe";
 import Livrancas from "./components/Livrancas/Livrancas";
@@ -14,6 +15,7 @@ function App() {
         <Navbar />
         <Container maxWidth="xl" sx={{ mt: 4 }}>
           <Routes>
+            <Route path='/' element={<Home />} />
             <Route path="/investment-credit" element={<InvestimentCredit />} />
             <Route path="/doe" element={<Doe />} />
             <Route path="/livrancas" element={<Livrancas />} />
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <img
+      style={{ width: 550, height: 550 }}
+      src={require('../../images/JRlogo_prev_ui.png')}
+      alt=""
+
+    />
+  </div>
+  )
+}
+
+export default Home
